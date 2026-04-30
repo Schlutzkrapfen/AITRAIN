810b3bc1663144b3f5960ab6634f9e6d35431351
Schlutzkrapfen <david.messner@protonmail.com>
2026-04-30 14:22:48 +0200

    added the function that images move to Trash

--- controll.py
+++ controll.py
@@ -17,8 +17,9 @@ def images_have_labels(image_files,label_files) :
     missing_labels =  []
     if unlabeled:
         for name in sorted(unlabeled):
-            print(f"Missing label: {name}")
-            missing_labels += name
+            full_path = next(input_dir.glob(f"{name}.*"), None)
+            print(f"Missing label: {full_path}")
+            missing_labels.append(full_path)
             
     else:
         print("All images have labels.")
@@ -39,7 +40,6 @@ def labels_have_images(image_files,label_files) :
         for name in sorted(unlabeled):
             print(f"Missing Image: {name}")
             missing_picture.append(Path(text_dir)/(name+".txt"))
-            
     else:
         print("All labels have Images.")
     return missing_picture
@@ -48,9 +48,8 @@ def move_to_trash_folder(paths,name="file"):
     for path in paths:
         if path.exists():
             trash_folder.mkdir(parents=True, exist_ok=True) 
-
             shutil.move(str(path), trash_folder / path.name)
-    print(f"moved every {name} that has no pair to the {trash_folder}")
+    print(f"moved every {name} that has no pair was moved to {trash_folder}")
 
 def main():
     images_path = get_images(input_dir)
