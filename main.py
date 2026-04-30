26a45728d59bf8b94727071587eaa033c3175fb7
Schlutzkrapfen <david.messner@protonmail.com>
2026-04-30 15:23:05 +0200

    fixed a bug with trashing for values

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ from pathlib import Path
 USER_DATA_DIR = 'user_data' 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
 from controll import get_images, get_text_files,images_have_labels,move_to_trash_folder, labels_have_images
+
 input_dir = Path("./InputFolder/images")
 text_dir = Path("./InputFolder/labels")
-trash_folder = Path("./InputFolder/Trash")
+trash_folder = Path("./Trash")
+
 def check():
     images_path = get_images(input_dir)
     if (not images_path  ):
@@ -25,7 +27,7 @@ def check():
         while True:
             choice = input(f"Missing {len(single_images)} label/s found. Remove (r) image/s, stop (n), or Continue (Y)? ").strip().lower()
             if choice == "r":
-                move_to_trash_folder(single_images, "image")
+                move_to_trash_folder(single_images,trash_folder, "image")
                 break
             elif choice == "n":
                 sys.exit(0)          
@@ -33,12 +35,13 @@ def check():
                 break
             else:
                 print("Invalid input, please enter r, n or y")
+    
     single_labels = labels_have_images(images_path,text_path,text_dir)
     if len(single_labels) > 0:
         while True:
             choice = input(f"Missing {len(single_labels)} image/s found. Remove (r) label/s, stop (n), or Continue (Y)? ").strip().lower()
             if choice == "r":
-                move_to_trash_folder(single_labels, "label")
+                move_to_trash_folder(single_labels,trash_folder ,"label")
                 break
             elif choice == "n":
                 sys.exit(0)          
@@ -47,5 +50,4 @@ def check():
             else:
                 print("Invalid input, please enter r, n or y")
         
-
 check()
