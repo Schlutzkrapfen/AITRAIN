80f726747f9d500e6beb3ebc064daaffdb1f5006
Schlutzkrapfen <david.messner@protonmail.com>
2026-04-30 09:15:25 +0200

    added the gitignore

--- split.py
+++ split.py
@@ -1,3 +1,17 @@
 import os
+import sys
+from os import listdir
 
 
+
+script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+# get the path or directory
+folder_dir = "./InputFolder"
+for images in os.listdir(folder_dir):
+
+    # check if the image ends with png or jpg or jpeg
+    if (images.endswith(".png") or images.endswith(".jpg")\
+        or images.endswith(".jpeg")):
+        # display
+        print(images)
